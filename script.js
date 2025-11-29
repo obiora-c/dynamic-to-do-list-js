29226cb3cb8f804f9a972610a516752de9663de6
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-11-29 05:26:34 -0800

    Od

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // Select DOM elements
-    const addButton = document.getElementById("Add Task");
+    const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
